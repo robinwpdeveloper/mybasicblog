e61dacaf19d81c93bb9b90948276ef4edf879667
Robin <robin@wpdeveloper.com>
2022-09-08 11:24:05 +0600

    comments file added

---
+++ comments.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * The template for displaying Comments
+ * 
+ * If the current post is protected by a password and the visitor has not yet
+ * entered the password we will return early without loading the comments.
+ */
+if ( post_password_required() ) {
+	return;
+}
+?>
+
+<div id="comments" class="comments-area">
+
+	<?php if ( have_comments() ) : ?>
+		<h2 class="comments-title">
+			<?php
+			if ( 1 === get_comments_number() ) {
+				printf(
+					/* translators: %s: The post title. */
+					_x( 'One thought on &ldquo;%s&rdquo;', 'comments title', 'mybasicblog' ),
+					'<span>' . get_the_title() . '</span>'
+				);
+			} else {
+				printf(
+					/* translators: 1: The number of comments, 2: The post title. */
+					_nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'mybasicblog' ),
+					number_format_i18n( get_comments_number() ),
+					'<span>' . get_the_title() . '</span>'
+				);
+			}
+			?>
+		</h2>
+
+		<ol class="comment-list">
+			<?php
+				wp_list_comments(
+					array(
+						'style'       => 'ol',
+						'short_ping'  => true,
+						'avatar_size' => 74,
+					)
+				);
+			?>
+		</ol><!-- .comment-list -->
+
+		<?php
+			// Are there comments to navigate through?
+		if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) :
+			?>
+		<nav class="navigation comment-navigation">
+		<h1 class="screen-reader-text section-heading"><?php _e( 'Comment navigation', 'mybasicblog' ); ?></h1>
+		<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'mybasicblog' ) ); ?></div>
+		<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'mybasicblog' ) ); ?></div>
+		</nav><!-- .comment-navigation -->
+		<?php endif; // Check for comment navigation. ?>
+
+		<?php if ( ! comments_open() && get_comments_number() ) : ?>
+		<p class="no-comments"><?php _e( 'Comments are closed.', 'mybasicblog' ); ?></p>
+		<?php endif; ?>
+
+	<?php endif; // have_comments() ?>
+
+	<?php comment_form(); ?>
+
+</div><!-- #comments -->
